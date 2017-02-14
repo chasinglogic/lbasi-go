0a7a2493fe11a3605d442f70d327a74bb8851025
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-14 15:50:30 -0500

    added a little test to main

--- lbasi.go
+++ lbasi.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chasinglogic/lbasi/interpreter"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	i := interpreter.New("3 + 4")
+	ans, err := i.Expr()
+
+	fmt.Printf("%v, %v\n", ans, err)
+
+	i = interpreter.New("10 + 1 + 2 - 3 + 4 + 6 - 15")
+	ans, err = i.Expr()
+
+	fmt.Printf("%v, %v\n", ans, err)
 }
