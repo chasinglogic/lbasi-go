ca35235bbd027950a37a6694d6ec00eaa8fc2de6
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-22 16:25:45 -0500

    created a main entry point

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/chasinglogic/lbasi/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey REPL\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
